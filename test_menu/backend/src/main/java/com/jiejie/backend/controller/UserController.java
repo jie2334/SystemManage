1efd9d0794dae5a884cc5a30b46e7e23fd99f98b
family_li <2278377651@qq.com>
2023-11-08 23:36:38 +0800

    security环境搭建

---
+++ test_menu/backend/src/main/java/com/jiejie/backend/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.jiejie.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0
+ * @description: TODO
+ * @author: 22783
+ * @date: 2023/11/8
+ **/
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+}
