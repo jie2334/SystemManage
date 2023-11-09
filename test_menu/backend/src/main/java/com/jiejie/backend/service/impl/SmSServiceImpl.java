c8d2ba1b2f0b6df69866f04d6a7f0ed1d3906a56
jack_jie <103995070+jie2334@users.noreply.github.com>
2023-11-09 16:40:55 +0800

    Update SmSServiceImpl.java

--- test_menu/backend/src/main/java/com/jiejie/backend/service/impl/SmSServiceImpl.java
+++ test_menu/backend/src/main/java/com/jiejie/backend/service/impl/SmSServiceImpl.java
@@ -73,7 +73,7 @@ public class SmSServiceImpl implements SmSService {
         String host = "https://gyytz.market.alicloudapi.com";
         String path = "/sms/smsSend";
         String method = "POST";
-        String appcode = "b1fe0d1273f84568ad57d173aafa58bf";
+        String appcode = "";
         Map<String, String> headers = new HashMap<>();
         //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
         headers.put("Authorization", "APPCODE " + appcode);
